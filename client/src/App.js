d7fe3ed6ea0703a59028437cf04d4cc3561eb792
aniruddhparwal <aniruddhparwal@gmail.com>
2021-03-04 23:16:06 +0530

    just tried

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container } from '@material-ui/core'
 
 function App() {
   return (
     <div className="App">
+      <Container maxWidth='lg'>
 
+      </Container>
     </div>
   );
 }
